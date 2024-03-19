768a1b7e99426023c6ccd552e380608b8a2f00c5
ioana-manghiuc <ioana-manghiuc@users.noreply.github.com>
2024-03-19 18:05:48 +0200

    Waits for user input (to check a word using the regular expr)

--- Automata_RegEx/Automata_RegEx/Main.cpp
+++ Automata_RegEx/Automata_RegEx/Main.cpp
@@ -8,14 +8,25 @@ int main()
 	RegEx rgx = FA.RegularExpression();
 	std::cout << "\nREGULAR EXPRESSION: " << rgx << std::endl;
 
-	if (rgx.CheckWord("abceeedab"))
-	{
-		std::cout << "\nword accepted\n";
-	}
-	else
-	{
-		std::cout << "\nword not accepted\n";
-	}
+    while (true)
+    {
+        std::string input;
+        std::cout << "\nWORD: ";
+        std::getline(std::cin, input);
+
+        if (input.empty()) {
+            break; 
+        }
+
+        if (rgx.CheckWord(input))
+        {
+            std::cout << " --> word accepted\n";
+        }
+        else
+        {
+            std::cout << " --> word NOT accepted\n";
+        }
+    }
 
 	return 0;
 }
